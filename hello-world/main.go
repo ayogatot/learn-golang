91d36c9b0d29fa2f55ab1854da48a588e5363b4f
ayogatot <azerino25@gmail.com>
2019-07-23 11:18:10 +0700

    fmt print using parameter

--- hello-world/main.go
+++ hello-world/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	// fmt.Println with no parameter
 	fmt.Println("Hello World !")
+
+	// fmt.Println with parameter
+	fmt.Println("This", "is", "fmt", "using", "parameter")
+	// can contains parameter and will be separate by "space"
 }
