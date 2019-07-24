c07f3ca7413ffefdfb51fdec2ea8436a9b3bd70f
ayogatot <azerino25@gmail.com>
2019-07-24 17:51:21 +0700

    adding many way too write variable in golang

---
+++ variable/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	// all variable must be used
+	// manifest typing = data type when declare variable must to be write
+	var number int
+	number = 10
+	fmt.Printf("Number : %d !\n", number)
+
+	// declaring variable without data type
+	var i, j = 1, 2
+	fmt.Println("Number2 :", i, j)
+
+	// using := to replace var
+	k := "OH YEAH"
+	fmt.Println(k)
+
+	// declaring variable using block
+	var (
+		l int    = 10
+		s string = "OKE"
+	)
+	fmt.Println(l, s)
+
+	// type inference to declare multiple variable
+	thisNumber, thisString, thisBool, thisFloat := 1, "Ini String", true, 2.2
+	fmt.Println(thisNumber, thisString, thisBool, thisFloat)
+}
