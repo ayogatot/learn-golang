27923fda616d17075628376f5c4f644fec503f69
ayogatot <azerino25@gmail.com>
2019-07-25 10:33:11 +0700

    Example of constant Variable

--- 4. Constanta/main.go
+++ 4. Constanta/main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Constanta")
+	// variable that cannot be changing after first initiation
+	const thisNumber, thisString, thisBool, thisFloat = 192, "Ya ini kan API", true, 129019.1281289
+	fmt.Println(thisNumber, thisBool, thisFloat, thisString)
+
+	const pi = 3.14
+	const jomblo = true
+	fmt.Printf("Kamu itu %.3f dan pi itu %t \n", pi, jomblo)
 }
