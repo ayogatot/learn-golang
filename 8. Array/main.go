552c14b38152fcfaaec83dfa798255cd65f48c25
ayogatot <azerino25@gmail.com>
2019-07-29 10:25:32 +0700

    looping array, for, for-range, `_` variable

--- 8. Array/main.go
+++ 8. Array/main.go
@@ -35,7 +35,30 @@ func main() {
 	// Array Multidimensi
 	var Array5 = [2][3]int{[3]int{1, 2, 3}, [3]int{4, 5, 6}}
 	fmt.Println("\nArray5 =>", Array5)
-	fmt.Println(Array5[0][0])
+	fmt.Printf("\n")
+
+	// Declare Array using Make
+	var Array6 = make([]string, 2)
+	Array6[0] = "Sanji"
+	Array6[1] = "Vinsmoke"
+	fmt.Println(Array6[0], Array6[1])
+	fmt.Printf("\n")
+
+	// Looping Array with for
+	for i := 0; i < len(Array4); i++ {
+		fmt.Printf("%s\n", Array4[i])
+	}
+	fmt.Printf("\n")
+
+	// Looping Array with for-range
+	for i, item := range Array4 {
+		fmt.Printf("Elemen ke %d = %s\n", i, item)
+	}
+	fmt.Printf("\n")
+	// alternative for-range
+	for _, item := range Array3 {
+		fmt.Printf("Isi Array3 : %s\n", item)
+	}
 
 	fmt.Println("\n============== Array ==============")
 
