4594c278054fcef96762dae9d46cb4f796de7df8
ayogatot <azerino25@gmail.com>
2019-07-31 17:43:04 +0700

    Function: Multiple return , defined and predefined

--- 11. Functions/main.go
+++ 11. Functions/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
@@ -16,9 +17,19 @@ func main() {
 	t := 5.1
 	fmt.Printf("Luas segitiga dengan alas %.2f dan tinggi %.2f adalah %.2f\n", a, t, luasSegitiga(a, t))
 
-	fmt.Printf("%d\n", divideNumber(6, 3))
+	fmt.Printf("\n%d\n", divideNumber(6, 3))
 	fmt.Printf("%d\n", divideNumber(2, 1))
 	fmt.Printf("%d\n", divideNumber(5, 0))
+
+	var diameter float64 = 15
+	var area, circumference = calculate(diameter)
+	var area2, circumference2 = calculate(diameter)
+
+	fmt.Printf("\nluas lingkaran\t\t: %.2f \n", area)
+	fmt.Printf("keliling lingkaran\t: %.2f \n", circumference)
+
+	fmt.Printf("\nluas lingkaran\t\t: %.2f \n", area2)
+	fmt.Printf("keliling lingkaran\t: %.2f \n", circumference2)
 
 	fmt.Println("\n============== Function ==============")
 
@@ -44,4 +55,22 @@ func divideNumber(a, b int) int {
 		return b
 	}
 	return a / b
+}
+
+// Functions with multiple return
+func calculate(d float64) (float64, float64) {
+	// hitung luas
+	var area = math.Pi * math.Pow(d/2, 2)
+	// hitung keliling
+	var circumference = math.Pi * d
+
+	// kembalikan 2 nilai
+	return area, circumference
+}
+
+func calculated(d float64) (circumference, area float64) {
+	area = math.Phi * math.Pow(d/2, 2)
+	circumference = math.Phi * d
+
+	return
 }
