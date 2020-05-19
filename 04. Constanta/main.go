436e6b123642a43514165c74be7a62bed8b6b1d8
ayogatot <azerino25@gmail.com>
2020-05-19 14:19:06 +0700

    trying use := while declaring variable using const

--- 04. Constanta/main.go
+++ 04. Constanta/main.go
@@ -8,7 +8,9 @@ func main() {
 	const thisNumber, thisString, thisBool, thisFloat = 192, "Ya ini kan API", true, 129019.1281289
 	fmt.Println(thisNumber, thisBool, thisFloat, thisString)
 
+	// declaring variable using const can't use :=
+	const numb string = "ohyeah"
 	const pi = 3.14
 	const jomblo = true
-	fmt.Printf("Kamu itu %.3f dan pi itu %t \n", pi, jomblo)
+	fmt.Printf("Kamu itu %.3f dan pi itu %t dan numb itu %s \n", pi, jomblo, numb)
 }
